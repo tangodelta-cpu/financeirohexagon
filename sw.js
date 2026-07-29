4ea0cffa4681fbd7d0cb87e537ffdbd7aa70b5c2
tangodelta-cpu <thalesndias@gmail.com>
2026-07-29 10:38:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'hexagon-finance-v1';
+const FILES = [
+  '/financeirohexagon/',
+  '/financeirohexagon/index.html',
+  '/financeirohexagon/manifest.json',
+  '/financeirohexagon/icon-192.png',
+  '/financeirohexagon/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
